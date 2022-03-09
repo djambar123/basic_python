8955ddf89e89297eeaa989eba561583405d1507b
djambar123 <djambar1994@gmail.com>
2022-03-10 00:45:06 +0200

    for loop

---
+++ basic.py
@@ -0,0 +1,24 @@
+# num = int(input("please enter a number:"))
+# while num > 0:
+#     import math
+#     print(math.pow(num,2))
+#     num = int(input("please enter a number:"))
+# else:
+#     print("invalid num")
+
+# num = int(input("please enter your age :"))
+# print('you will be',num+20,'in 20 years')
+
+# num = int(input("please enter price to add tax :"))
+# print('your price is:',(num*0.17) + num)
+
+# num1 = int(input("enter first number:"))
+# num2 = int(input("enter seconed number:"))
+# plus = num1 + num2
+# minus = num1 - num2
+# tims = num1*num2
+# mana = num1 / num2
+# print (plus,minus,tims,mana)
+
+
+
