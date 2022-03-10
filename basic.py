59833225be0d82e227bf306245eee345912a1298
djambar123 <djambar1994@gmail.com>
2022-03-10 22:18:39 +0200

    for loop

--- basic.py
+++ basic.py
@@ -29,7 +29,20 @@
 # num2 = int(input("enter number b:"))
 # num3 = int(input("enter number c:"))
 # num4 = int(input("enter number d:"))
-# print (num1+num2+num3+num4 /4)
+# print("the averege is :"num1+num2+num3+num4 /4)
 
 
-nails = int(input("how many nails:"))
+# nails = int(input("how many nails:"))
+# box =  nails / 40
+# print (box)
+
+# distance= int(input("enter kilomerter :"))
+# speed = int(input("in what speed:"))
+# time = distance / speed
+# print (time)
+
+# phone = int(input("enter phone number without 0 :"))
+# num = str(int(phone))
+# print("your number is:", "0" +num)
+
+
